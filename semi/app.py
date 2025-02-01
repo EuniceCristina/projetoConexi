1b67cac25b17da12286408819ad320ef1b30b36c
Eunice <eunicecristina0510@gmail.com>
2025-02-01 13:07:56 -0300

    commit

--- semi/app.py
+++ semi/app.py
@@ -22,7 +22,17 @@ def index():
     global acertos
     global texto
     cor_muda=False
-    
+    reinicia = request.args.get('reinicia', 'false').lower() == 'true'
+    if reinicia:
+        var = []
+        acertos = 0
+        cor_1='rgb(224, 243, 250)'
+        cor_2='rgb(224, 243, 250)'
+        cor_3='rgb(224, 243, 250)'
+        cor_4='rgb(224, 243, 250)'
+        cor_5='rgb(224, 243, 250)'
+        texto=''
+        return redirect(url_for('index'))
 
     if request.method=='POST':
         cor = request.form['palavra']
@@ -55,9 +65,20 @@ def index():
             texto=render_template_string(f'<h3>Itens selecionados : {len(var)}</h3>')
         if acertos==5:
             var = []
-            texto ='<h3 style="color: cadeblue; font-weight:800;">Você ganhou o jogo!</h3>'
+            texto = '<button style="width: 100%; background-color: green; height:40px; color: white;font-weight: 800;">Você ganhou o jogo!</button>'
            
         return render_template('index.html',cor_5=cor_5,cor_4=cor_4,cor_3=cor_3,cor_2=cor_2,cor_1=cor_1, texto=texto, acertos=acertos,cor_muda=cor_muda)
     
+        
+    
     return render_template('index.html')    
+
+@app.route('/regras',methods=['GET','POST'])
+def regras():
+    return render_template('regras.html')
+
+@app.route('/reinicia')
+def reinicia():
+    reinicia = True
+    return redirect(url_for('index',reinicia=reinicia))
             
